067c2a5f1bf36c6fadbca2737e5260aa3e278cd7
satya <sunildonipathi@gmail.com>
2020-02-14 20:14:20 +0530

    MVC

---
+++ 14-02-2020(MVC)/WebApplication1/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+
+        private List<Employee> _emploloyeeList;
+        public MockEmployeeRepository()
+        {
+            _emploloyeeList = new List<Employee>()
+            {
+                new Employee() {Id = 1, Name = "Marry", Department="Hr", Email="marry@gmail.com"},
+                new Employee() {Id = 2, Name = "Harry", Department="Dev", Email="harry@gmail.com"},
+                new Employee() {Id = 3, Name = "Sam", Department="QA", Email="sam@gmail.com"},
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _emploloyeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
---
+++ 14-02-2020(MVC)/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository; 
+        }
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(2).Email;
+        }
+        public JsonResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            return Json(model);
+        }
+    }
+}
