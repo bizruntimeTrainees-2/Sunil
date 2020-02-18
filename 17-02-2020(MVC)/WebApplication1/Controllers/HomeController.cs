d79783754f7c7b3a27cb6c6bcf4457e0cc75822c
satya <sunildonipathi@gmail.com>
2020-02-18 09:25:11 +0530

    MVC

---
+++ 17-02-2020(MVC)/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository; 
+        }
+        public string Index()
+        {
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            // return View(model);
+            //return View("Myviews/Test.cshtml");
+            return View("Views/Home/Test.cshtml");
+            // or we can specify directly
+            // return View("Test");
+        }
+    }
+}
---
+++ 17-02-2020(MVC)/WebApplication1/Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employees;
+        public MockEmployeeRepository()
+        {
+            _employees = new List<Employee>()
+            {
+                new  Employee(){Id=1, Name="Harry", Designation="Hr", Email="harry@gmail.com"},
+                new  Employee(){Id=1, Name="Marry", Designation="DEV", Email="marry@gmail.com"},
+                new  Employee(){Id=1, Name="Jokey", Designation="QA", Email="jokey@gmail.com"},
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employees.FirstOrDefault<Employee>();
+        }
+        
+    }
+}
