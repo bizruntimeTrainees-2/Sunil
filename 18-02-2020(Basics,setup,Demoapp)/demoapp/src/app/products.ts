1e1afc4a6fafa271b6cd03c89e03a25efaeda2db
satya <sunildonipathi@gmail.com>
2020-02-20 09:31:42 +0530

    Angular

---
+++ 18-02-2020(Basics,setup,Demoapp)/demoapp/src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Vivo',
+    price: 15000,
+    description: 'Vivo phone is one of the best phone in low price'
+  },
+  {
+    name: 'Samsung',
+    price: 20000,
+    description: 'A great phone with one of the best display'
+  },
+  {
+    name: 'IPhone',
+    price: 50000,
+    description: 'Every thing is best'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
